def75da1b2ebca5180c7309cfebf512e6828ce26
Vivek Singh <vivekkmr45@yahoo.in>
2018-04-13 15:41:39 +0530

    added comments

--- worker.go
+++ worker.go
@@ -103,16 +103,22 @@ func StartDispatcher(nworkers int) {
 	// Common WorkeQueue for all workers to comsume the work
 	WorkerQueue := make(chan chan WorkRequest, nworkers)
 
+	//Start all the workers
 	for i := 0; i < nworkers; i++ {
 		worker := NewWorker(i+1, WorkerQueue)
 		worker.Start()
 	}
 
+	//Start dispatching the work requests
 	go func() {
 		for {
 			select {
+
+			// Recieve work request
 			case work := <-WorkQueue:
 				fmt.Println("Recieved work request")
+
+				// Dispatch the work request
 				go func() {
 					worker := <-WorkerQueue
 					fmt.Println("Dispatching the work request")
